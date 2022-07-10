30972c11f8de84d0ff5605ea34481124dc9edd80
Connor Henderson <connor.henderson@talkiatry.com>
2022-07-10 16:51:19 -0400

    added nn methods

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,18 +1,73 @@
 import numpy as np
 
-my_data = np.genfromtxt('../data/train.csv',
-                        delimiter=',',
-                        autostrip=True,
-                        skip_header=1,
-                        usecols=(2, 5, 6, 7, 8, 10),
-                        encoding='UTF-8',
-                        converters={
-                            2: lambda s: int(s or 0),
-                            5: lambda s: 1 if "female" in s else 0,
-                            6: lambda s: int(s or 0),
-                            7: lambda s: int(s or 0),
-                            8: lambda s: int(s or 0),
-                            10: lambda s: int(s or 0)
-                        })
-for i in range(20):
-    print(my_data[i])
+
+def embarked(s):
+    if "Q" in s:
+        return 1
+    elif "C" in s:
+        return 2
+    else:
+        return 3
+
+
+# TRAINING DATA
+imported_training_data = np.genfromtxt('data/train.csv',
+                                       delimiter=',',
+                                       autostrip=True,
+                                       skip_header=1,
+                                       usecols=(2, 5, 6, 7, 8, 10),
+                                       encoding='UTF-8',
+                                       converters={
+                                           2: lambda s: int(s or 0),
+                                           5: lambda s: 1 if "female" in s else 0,
+                                           6: lambda s: round(float(s or 0), 1),
+                                           7: lambda s: int(s or 0),
+                                           8: lambda s: int(s or 0),
+                                           10: lambda s: round(float(s or 0), 2),
+                                           12: embarked,
+                                       })
+
+array_training_data = []
+for i, row in enumerate(imported_training_data):
+    array_training_data.append([*row])
+
+# FOR EXPORT
+training_solution = np.genfromtxt('data/train.csv',
+                                  delimiter=',',
+                                  skip_header=1,
+                                  usecols=(1),
+                                  converters={1: lambda s: int(s or 0)}
+                                  )
+training_data = list(zip(np.array(array_training_data), training_solution))
+# we are exporting an array of length two, with each entry being a several hundred set of data
+# instead, we should zip these together so we export a several hundred line array with zipped lines
+
+# TEST DATA
+imported_test_data = np.genfromtxt('data/train.csv',
+                                   delimiter=',',
+                                   autostrip=True,
+                                   skip_header=1,
+                                   usecols=(2, 5, 6, 7, 8, 10, 12),
+                                   encoding='UTF-8',
+                                   converters={
+                                       2: lambda s: int(s or 0),
+                                       5: lambda s: 1 if "female" in s else 0,
+                                       6: lambda s: round(float(s or 0), 1),
+                                       7: lambda s: int(s or 0),
+                                       8: lambda s: int(s or 0),
+                                       10: lambda s: round(float(s or 0), 2),
+                                       12: embarked,
+                                   })
+
+array_test_data = []
+for i, row in enumerate(imported_test_data):
+    array_test_data.append([*row])
+
+# FOR EXPORT
+test_solution = np.genfromtxt('data/test.csv',
+                              delimiter=',',
+                              skip_header=1,
+                              usecols=(1),
+                              converters={1: lambda s: int(s or 0)}
+                              )
+test_data = list(zip(np.array(array_test_data), test_solution))
--- src/network.py
+++ src/network.py
@@ -1,2 +1,85 @@
-# class Network:
-#     def __init__():
+import numpy as np
+from clean_data import training_data, test_data
+
+# the test file has no solution... has to be submitted to evaluate it.
+
+# SGD on ReLu neurons
+
+
+class Network:
+    def __init__(self, sizes):
+        self.num_layers = sizes
+        self.sizes = sizes
+        self.weights = [np.random.randn(x, y)
+                        for y, x in zip(sizes[:-1], sizes[1:])]
+        self.biases = [np.zeros(shape=(y, 1)) for y in sizes[1:]]
+
+    def SGD(self, training_data, lr, epochs, mini_batch_size):
+        for epoch in range(epochs):
+            np.random.shuffle(training_data)
+            for mb_idx in range(0, len(training_data), mini_batch_size):
+                mini_batch = training_data[mb_idx:mb_idx+mini_batch_size]
+                self.update_mini_batch(mini_batch, lr)
+
+    def sigmoid(self, z):
+        return 1.0/(1.0+np.exp(-z))
+
+    def sigmoid_prime(self, z):
+        return self.sigmoid(z)*(1-self.sigmoid(z))
+
+    def cost_derivative(self, output_activations, y):
+        return (output_activations-y)
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # feedforward
+        activation = x
+        activations = [x]
+        zs = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = self.sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * \
+            self.sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = self.sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def update_mini_batch(self, mini_batch, lr):
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        nabla_b = [np.zeros(b.shape for b in self.biases)]
+        for passenger, survival in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(passenger, survival)
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+        self.weights = [w-(lr/len(mini_batch))*nw for w,
+                        nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(lr/len(mini_batch))*nb for b,
+                       nb in zip(self.biases, nabla_b)]
+
+    # def feedforward(self, data):
+    #     pass
+
+    # def evaluate(self, eval_data):
+    #     eval_input, eval_solution = eval_data
+    #     eval_results = self.feedforward(eval_input)
+    #     t = sum(x == y for x, y in zip(eval_results, eval_solution))
+    #     return t
+
+
+a = Network([6, 20, 20, 1])
+# a.evaluate(training_data)
+a.SGD(training_data=training_data, lr=1,
+      epochs=10, mini_batch_size=100)
